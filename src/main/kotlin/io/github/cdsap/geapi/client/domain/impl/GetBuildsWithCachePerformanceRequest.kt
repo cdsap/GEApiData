6a7cfab893af1099ca8d4a8de203b8e5bbced610
Inaki Villar <inaki.seri@gmail.com>
2026-08-26 12:39:15 -0700

    fix: refactor scan metadata enrichment to clarify mapping boundary

--- src/main/kotlin/io/github/cdsap/geapi/client/domain/impl/GetBuildsWithCachePerformanceRequest.kt
+++ src/main/kotlin/io/github/cdsap/geapi/client/domain/impl/GetBuildsWithCachePerformanceRequest.kt
@@ -2,6 +2,7 @@ package io.github.cdsap.geapi.client.domain.impl
 
 import io.github.cdsap.geapi.client.domain.GetBuildsWithCachePerformance
 import io.github.cdsap.geapi.client.domain.impl.logger.Logger
+import io.github.cdsap.geapi.client.domain.impl.mapper.ScanMapper
 import io.github.cdsap.geapi.client.domain.impl.progress.ProgressFeedback
 import io.github.cdsap.geapi.client.model.Build
 import io.github.cdsap.geapi.client.model.Filter
@@ -37,6 +38,7 @@ class GetBuildsWithCachePerformanceRequest(private val repository: GradleEnterpr
         val duration = System.currentTimeMillis().toDuration(DurationUnit.MILLISECONDS)
         val progressFeedback = ProgressFeedback(filter.clientType, builds.size)
         val semaphore = Semaphore(filter.concurrentCallsConservative)
+        val scanMapper = ScanMapper()
 
         progressFeedback.init()
 
@@ -51,7 +53,7 @@ class GetBuildsWithCachePerformanceRequest(private val repository: GradleEnterpr
                             } else {
                                 repository.getBuildScanMavenCachePerformance(it.id)
                             }.apply {
-                                map(this, it)
+                                scanMapper.enrichBuildWithScanMetadata(this, it)
                             }
                         progressFeedback.update()
                         semaphore.release()
@@ -67,18 +69,5 @@ class GetBuildsWithCachePerformanceRequest(private val repository: GradleEnterpr
             ),
         )
         return cachePerformanceBuild
-    }
-
-    private fun map(
-        cachePerformance: Build,
-        scan: ScanWithAttributes,
-    ) {
-        cachePerformance.builtTool = scan.buildTool
-        cachePerformance.buildStartTime = scan.buildStartTime
-        cachePerformance.tags = scan.tags
-        cachePerformance.projectName = scan.projectName
-        cachePerformance.requestedTask = scan.requestedTasksGoals
-        cachePerformance.buildDuration = scan.buildDuration
-        cachePerformance.values = scan.values
     }
 }
